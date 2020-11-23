9b755335c9fe3c1ad981ba3c20d478407a58d911
Klaudia Paprika <kpaprika@ab.bluecross.ca>
2020-11-23 12:18:14 +0100

    Initial Commit

---
+++ iOS-Intermediate-lession-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Intermediate-lession-1
+//
+//  Created by klaudia paprika on 11/23/20.
+//  Copyright © 2020 klaudia paprika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS-Intermediate-lession-1Tests/iOS_Intermediate_lession_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_Intermediate_lession_1Tests.swift
+//  iOS-Intermediate-lession-1Tests
+//
+//  Created by klaudia paprika on 11/23/20.
+//  Copyright © 2020 klaudia paprika. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_Intermediate_lession_1
+
+class iOS_Intermediate_lession_1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
